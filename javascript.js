09fcb6ab2a92db7dcbd18a638068bffdcad61f3c
bissour <bissour1983@gmail.com>
2020-05-09 01:26:31 +0000

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,179 @@
+var js_region=[
+
+	
+	{
+	"titre":"nord pas de calais",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"picardie",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"haute normandie",
+	"contenu":'le gros horloge se situant a rouen dans la rue pietonne portant le meme nom',
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"basse normandie",
+	"contenu":"bonjour tout le monde",
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"champagne ardenne",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"lorraine",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+
+	{
+	"titre":"ile de france",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+
+	{
+	"titre":"alsace",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+
+	{
+	"titre":"bretagne",
+	"contenu":false,
+	"classe":"cadre",
+	"image":"image/mont_sm.jpg"
+
+	},
+	
+	{
+	"titre":"centre",
+	"contenu":false,
+	"classe":"cadre",
+	"image":"image/chambord.jpg"
+
+	},
+	
+	{
+	"titre":"bourgogne",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"franche compte",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"poitou charente",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"limousin",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"rhone-alpe",
+	"contenu":"le massif du mont blanc est le plus haut sommet d'Europe",
+	"classe":"cadre",
+	"image":"image/mont_blanc.jpg"
+
+	},
+	
+	{
+	"titre":"pays de la loire",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"auvergne",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"provence alpes cote d'azure",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"languedoc-roussillon",
+	"contenu":false,
+	"classe":"cadre",
+	"image":"image/theatre_orange.jpg"
+
+	},
+	
+	{
+	"titre":"midi-pyrenees",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	},
+	
+	{
+	"titre":"aquitaine",
+	"contenu":"la grottes de lascaux et ses peintures datant d'environ 17 000 ans",
+	"classe":"cadre",
+	"image":"image/Lascaux.jpg"
+
+	},
+	
+	{
+	"titre":"corse",
+	"contenu":false,
+	"classe":"cadre",
+	"image":false
+
+	}
+]
